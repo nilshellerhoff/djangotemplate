6395d3ae42f5376a0d85430e33852891f7b6f2e3
nilshellerhoff <nils.hellerhoff@gmail.com>
2023-12-10 17:10:40 +0100

    django template with vue + vuetify

---
+++ djangotemplate/templates/djangotemplate/vue_components/app_bar.vue
@@ -0,0 +1,56 @@
+<template id="templateAppBar">
+  <v-app-bar
+      color="primary"
+      prominent
+  >
+    <v-app-bar-nav-icon variant="text"
+                        @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+    <v-toolbar-title>[[ title ]]</v-toolbar-title>
+
+  </v-app-bar>
+
+  <v-navigation-drawer
+      v-model="drawer"
+  >
+    <v-list>
+      <v-list-item
+          v-for="link in links"
+          :key="link.value"
+          :value="link"
+          color="primary"
+          :href="link.href"
+      >
+        <template v-slot:prepend>
+          <v-icon :icon="link.icon"></v-icon>
+        </template>
+        <!--        <a :href="link.href">-->
+        <v-list-item-title v-text="link.title"></v-list-item-title>
+        <!--        </a>-->
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+const appBar = {
+  delimiters: ['[[', ']]'],
+  template: '#templateAppBar',
+  props: ['title'],
+  data() {
+    return {
+      links: [{
+        value: "home",
+        title: "Home",
+        href: "/",
+        icon: "mdi-home"
+      },
+      ],
+      drawer: null,
+    }
+  },
+  computed: {}
+}
+
+app.component('appBar', appBar);
+</script>
---
+++ djangotemplate/templatetags/variable.py
@@ -0,0 +1,21 @@
+import json
+from django import template
+from django.db.models import QuerySet
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+@register.filter
+
+def variable(value, name):
+    if type(value) == QuerySet:
+        value = list(value)
+
+    json_str = json.dumps(value)
+    # second encoding encodes quotes
+    js_str = json.dumps(json_str)
+    result = f"""
+<script>
+    var {name} = JSON.parse({js_str});
+</script>"""
+    return mark_safe(result)
